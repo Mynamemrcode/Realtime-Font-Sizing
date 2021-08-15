aab7ebfdfd7ee6aad00314400f5ddf8669f00a93
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-08-15 14:32:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+
+}
+
+function setup() {
+canvas = createCanvas(650,480);
+canvas.position(950,200);
+video = createCapture(VIDEO);
+video.position(200,200);
+}
+
+function draw() {
+}
