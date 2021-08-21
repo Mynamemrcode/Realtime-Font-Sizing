b023f06f8a0ebf3f57991c2103d62b0bf25cd78f
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-08-21 16:48:47 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+Lwrist= 0;
+Rwrist= 0;
+nosex= 0;
+nosey= 0;
+diifference = 0;
 function preload() {
 
 }
@@ -7,7 +12,21 @@ canvas = createCanvas(650,480);
 canvas.position(950,200);
 video = createCapture(VIDEO);
 video.position(200,200);
+
+poser = ml5.poseNet(video, Load);
+
+poser.on('pose', Gotem());
 }
 
 function draw() {
+}
+
+function Load() {
+    console.log("🪵loaded");
+}
+
+function Gotem(results) {
+    if(results.length >0) {
+        console.log(results);
+    }
 }
